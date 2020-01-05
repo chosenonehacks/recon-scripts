4337b32051c4fa5a918a85120dbf738308d43391
chosenonepl <hal9k2@hotmail.com>
2020-01-05 07:21:08 -0500

    init

---
+++ recon.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+#2020 @chosenonepl
+#Script for initial recon
+#1. For subdomains two tools: assetfinder and amass
+#2. For live check httprobe
+#3. For searching old urls 
+#4. For possibile subdomain takeover used tool called subjack
+#5. For website images gowitness
+
+#Requiments GO and installed tools
+#Config before u start
+#apt install golang
+#go version
+#set proper $PATH variables i.e.
+#echo $PATH
+#/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/go/bin:/root/go/bin
+#go get -u github.com/tomnomnom/assetfinder
+#go get -v -u github.com/OWASP/Amass/v3/...
+#go get -u github.com/tomnomnom/httprobe
+#go get -u github.com/tomnomnom/waybackurls
+#go get -u github.com/haccer/subjack
+#go get -u github.com/sensepost/gowitness
+
+url=$1
+if [ ! -d "$url" ];then
+	mkdir $url
+fi
+if [ ! -d "$url/recon" ];then
+	mkdir $url/recon
+fi
+    if [ ! -d '$url/recon/gowitness' ];then
+        mkdir $url/recon/gowitness
+    fi
+	if [ ! -d '$url/recon/gowitness/http' ];then
+        mkdir $url/recon/gowitness/http
+    fi
+	if [ ! -d '$url/recon/gowitness/https' ];then
+        mkdir $url/recon/gowitness/https
+    fi
+if [ ! -d "$url/recon/scans" ];then
+	mkdir $url/recon/scans
+fi
+if [ ! -d "$url/recon/httprobe" ];then
+	mkdir $url/recon/httprobe
+fi
+if [ ! -d "$url/recon/potential_takeovers" ];then
+	mkdir $url/recon/potential_takeovers
+fi
+if [ ! -d "$url/recon/wayback" ];then
+	mkdir $url/recon/wayback
+fi
+if [ ! -d "$url/recon/wayback/params" ];then
+	mkdir $url/recon/wayback/params
+fi
+if [ ! -d "$url/recon/wayback/extensions" ];then
+	mkdir $url/recon/wayback/extensions
+fi
+if [ ! -f "$url/recon/httprobe/alive.txt" ];then
+	touch $url/recon/httprobe/alive.txt
+fi
+if [ ! -f "$url/recon/final.txt" ];then
+	touch $url/recon/final.txt
+fi
+
+echo "[+] Harvesting subdomains with assetfinder..."
+assetfinder $url >> $url/recon/assets.txt
+cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
+rm $url/recon/assets.txt
+
+echo "[+] Double checking for subdomains with amass..."
+amass enum -d $url >> $url/recon/f.txt
+sort -u $url/recon/f.txt >> $url/recon/final.txt
+rm $url/recon/f.txt
+
+echo "[+] Probing for alive domains..."
+cat $url/recon/final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' | tee -a $url/recon/httprobe/a.txt
+sort -u $url/recon/httprobe/a.txt > $url/recon/httprobe/alive.txt
+rm $url/recon/httprobe/a.txt
+
+echo "[+] Checking for possible subdomain takeover..."
+
+if [ ! -f "$url/recon/potential_takeovers/potential_takeovers.txt" ];then
+	touch $url/recon/potential_takeovers/potential_takeovers.txt
+fi
+
+subjack -w $url/recon/final.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/recon/potential_takeovers/potential_takeovers.txt
+
+echo "[+] Scanning for open ports..."
+nmap -iL $url/recon/httprobe/alive.txt -T4 -oA $url/recon/scans/scanned.txt
+
+echo "[+] Scraping wayback data..."
+cat $url/recon/final.txt | waybackurls >> $url/recon/wayback/wayback_output.txt
+sort -u $url/recon/wayback/wayback_output.txt
+
+echo "[+] Pulling and compiling all possible params found in wayback data..."
+cat $url/recon/wayback/wayback_output.txt | grep '?*=' | cut -d '=' -f 1 | sort -u >> $url/recon/wayback/params/wayback_params.txt
+for line in $(cat $url/recon/wayback/params/wayback_params.txt);do echo $line'=';done
+
+echo "[+] Pulling and compiling js/php/aspx/jsp/json files from wayback output..."
+for line in $(cat $url/recon/wayback/wayback_output.txt);do
+	ext="${line##*.}"
+	if [[ "$ext" == "js" ]]; then
+		echo $line >> $url/recon/wayback/extensions/js1.txt
+		sort -u $url/recon/wayback/extensions/js1.txt >> $url/recon/wayback/extensions/js.txt
+	fi
+	if [[ "$ext" == "html" ]];then
+		echo $line >> $url/recon/wayback/extensions/jsp1.txt
+		sort -u $url/recon/wayback/extensions/jsp1.txt >> $url/recon/wayback/extensions/jsp.txt
+	fi
+	if [[ "$ext" == "json" ]];then
+		echo $line >> $url/recon/wayback/extensions/json1.txt
+		sort -u $url/recon/wayback/extensions/json1.txt >> $url/recon/wayback/extensions/json.txt
+	fi
+	if [[ "$ext" == "php" ]];then
+		echo $line >> $url/recon/wayback/extensions/php1.txt
+		sort -u $url/recon/wayback/extensions/php1.txt >> $url/recon/wayback/extensions/php.txt
+	fi
+	if [[ "$ext" == "aspx" ]];then
+		echo $line >> $url/recon/wayback/extensions/aspx1.txt
+		sort -u $url/recon/wayback/extensions/aspx1.txt >> $url/recon/wayback/extensions/aspx.txt
+	fi
+done
+
+rm $url/recon/wayback/extensions/js1.txt
+rm $url/recon/wayback/extensions/jsp1.txt
+rm $url/recon/wayback/extensions/json1.txt
+rm $url/recon/wayback/extensions/php1.txt
+rm $url/recon/wayback/extensions/aspx1.txt
+
+echo "[+] Running gowitness against all compiled domains..."
+#tried based on nmap dicovered services but results were poor
+#gowitness nmap -f myatos.net/recon/scans/scanned.txt.xml -d myatos.net/recon/gowitness
+gowitness file -s $url/recon/httprobe/alive.txt --prefix-https -d $url/recon/gowitness/https
+gowitness file -s $url/recon/httprobe/alive.txt --prefix-http -d $url/recon/gowitness/http
